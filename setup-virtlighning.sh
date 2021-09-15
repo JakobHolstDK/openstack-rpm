5a328c3cef663035783f23aee59378ba6c27d0ee
Jakob Holst <jho@miracle.dk>
2021-09-15 10:26:44 +0200

    setup virt-lightening

--- setup-virtlighning.sh
+++ setup-virtlighning.sh
@@ -1,1 +1,27 @@
-#!/usr/bin/
+#!/usr/bin/env bash
+
+vl down
+
+
+#Here we could setup virt-lightning.yaml
+#Here redhat cleanup could and should cleanup orphans in the rhel subscriptions
+
+
+vl up
+
+echo "#Auto generated inventory for virt-lightening" > inventory.remote
+
+echo "" >> inventory.remote
+echo "[api]" >> inventory.remote 
+vl ansible_inventory | grep api >> inventory.remote
+
+echo "" >> inventory.remote
+echo "[db]" >> inventory.remote 
+vl ansible_inventory | grep mariadb >> inventory.remote
+
+echo "" >> inventory.remote
+echo "[compute]" >> inventory.remote 
+vl ansible_inventory | grep compute >> inventory.remote
+echo "" >> inventory.remote
+echo "" >> inventory.remote
+echo "" >> inventory.remote
