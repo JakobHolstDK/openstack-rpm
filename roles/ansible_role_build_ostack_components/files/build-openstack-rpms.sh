94a36ff1e5e3b2fe5830525ffd39c33b45d46a4c
Jakob Holst <jho@miracle.dk>
2021-09-17 13:20:23 +0200

    spelling

--- roles/ansible_role_build_ostack_components/files/build-openstack-rpms.sh
+++ roles/ansible_role_build_ostack_components/files/build-openstack-rpms.sh
@@ -5,10 +5,24 @@ PKDIR="/var/lib/ostack/packages/"
 for MODULE in $(ls -1  $PKDIR)
 do
     echo "`date`: finding source packages in $MODULE"
-    for DIR in $(tree -L 1 -d  |awk '{ print $2 }'|grep  -v directory|grep -i [a-z])
+    for DIR in $(tree -L 1 -d  |awk '{ print $2 }'|grep  -iv "directory|directories" |grep -i [a-z])
     do  
         cd ${PKDIR}/${MODULE}/${DIR}
-        cat setup.cfg |grep -i "name"
+        if [[ $? == 0 ]];
+        if [[ -f setup.cfg ]];
+        then    
+            echo "`date`: Setup cfg file is missing"
+            echo "name = openstackrpm-${MODULE}"
+        else
+            grep -i "^name =" setup.cfg >/dev/null 2>&1
+            if [[ $? == 0 ]];
+            then
+                echo "`date`: We have the entry in the file"
+            else
+                exit
+            fi
+        fi
+        grep -i "^name =" setup.cfg 2>/dev/null
     done
 done
 
