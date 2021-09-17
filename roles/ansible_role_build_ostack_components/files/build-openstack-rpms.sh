f1c24fe63e181e76643d3a01ef357b994f2e1cf6
Jakob Holst <jho@miracle.dk>
2021-09-17 13:52:32 +0200

    spelling

--- roles/ansible_role_build_ostack_components/files/build-openstack-rpms.sh
+++ roles/ansible_role_build_ostack_components/files/build-openstack-rpms.sh
@@ -1,28 +1,52 @@
 #!/usr/bin/env bash
 
+
 PKDIR="/var/lib/ostack/packages/"
 
 for MODULE in $(ls -1  $PKDIR)
 do
     echo "`date`: finding source packages in $MODULE"
-    for DIR in $(tree -L 1 -d  |awk '{ print $2 }'|grep  -iv "directory|directories" |grep -i [a-z])
+    for MYDIR in $(tree -L 1 -d ${PKDIR}/${MODULE} |awk '{ print $2 }'|egrep  -iv "directory|directories" |grep -i [a-z])
     do  
-        cd ${PKDIR}/${MODULE}/${DIR}
+        cd ${PKDIR}/${MODULE}/${MYDIR}
         if [[ $? == 0 ]];
-        if [[ -f setup.cfg ]];
-        then    
+        then
+          echo "`date`: changedir"
+       	  if [[ ! -f setup.cfg ]];
+          then    
             echo "`date`: Setup cfg file is missing"
-            echo "name = openstackrpm-${MODULE}"
-        else
-            grep -i "^name =" setup.cfg >/dev/null 2>&1
+            echo "[metadata]" > setup.cfg
+            echo "name = opestackrpm-${MODULE}" >> setup.cfg
+	  fi
+
+          grep -i "name =" setup.cfg >/dev/null 2>&1
+          if [[ $? == 0 ]];
+          then
+            echo "`date`: We have the entry in the file"
+            grep -v "name = openstackrpm-" setup.cfg >/dev/null 2>&1
             if [[ $? == 0 ]];
             then
-                echo "`date`: We have the entry in the file"
-            else
-                exit
+		sed -i "s/name = /name = openstackrpm-/" setup.cfg >/dev/null 2>&1
             fi
-        fi
-        grep -i "^name =" setup.cfg 2>/dev/null
+          else
+            echo "`date`: We are missing the entry in the file"
+	    grep "\[metadate\]" setup.cfg >/dev/null 2>&1
+            if [[ $? == 0 ]]; 
+            then
+                sed "s/[metadata]/[metadata]\name = opestackrpm-${MODULE}/"
+            else
+		mv setup.cfg setup.cfg.save
+                echo "[metadata]" > setup.cfg
+                echo "name = opestackrpm-${MODULE}" >> setup.cfg
+                cat setup.cfg.save >> setup.cfg
+            fi
+          fi
+         grep "name = openstackrpm-" setup.cfg >/dev/null 2>&1
+	if [[ $? == 0 ]];
+        then
+	    python setup.py bdist_rpm
+	fi
+
+       fi
     done
 done
-
